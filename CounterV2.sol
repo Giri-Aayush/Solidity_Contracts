94403a6d3290185193f8e4fb53d5247c4248a71e
Aayush Giri <aayushgiri1234@gmail.com>
2022-12-30 22:25:57 +0530

    Counter Contract V2

---
+++ CounterV2.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter{
+
+    uint public count = 0;
+
+   // we can remove the constructor and directly specify the public variable
+
+    function getCount() public view returns(uint){
+        return count;
+    }
+
+    function incrementCount() public {
+        count++;
+    }
+
+    function decrementCount() public {
+        count--;
+    }
+
+}
